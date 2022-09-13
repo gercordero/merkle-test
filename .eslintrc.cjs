d5f308e410586eea70190a2490e00fd35f91637c
German Cordero <gerrcordero@gmail.com>
2022-09-13 23:41:48 +0200

    Updating rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,12 +18,15 @@ module.exports = {
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:react/recommended",
     "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
     "prettier",
   ],
   plugins: ["@typescript-eslint", "react", "react-hooks", "jsx-a11y"],
   rules: {
     "@typescript-eslint/no-explicit-any": "off", // any has its uses ;)
     "@typescript-eslint/ban-ts-comment": "off", // we want to be able to use @ts-ignore
+    "@typescript-eslint/promise-function-async": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
     "react/react-in-jsx-scope": "off",
     "no-console": "warn",
   },
