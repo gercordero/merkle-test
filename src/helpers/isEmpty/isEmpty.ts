63e6a235c520cc19ce653d00fefee98af3212493
German Cordero <gerrcordero@gmail.com>
2022-09-13 23:42:50 +0200

    Removing comment

--- src/helpers/isEmpty/isEmpty.ts
+++ src/helpers/isEmpty/isEmpty.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
-
 const isEmpty = (value: any): boolean =>
   [Object, Array].includes((value || {})?.constructor) &&
   Object.entries(value || {}).length === 0;
