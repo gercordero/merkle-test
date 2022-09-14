5c3ed46fdebf32b3b5a37e14388316e5111d37f6
German Cordero <gerrcordero@gmail.com>
2022-09-14 18:14:34 +0200

    Adding getSortedItemsByScore function

--- src/services/itemService/itemService.ts
+++ src/services/itemService/itemService.ts
@@ -12,5 +12,18 @@ export const getItem = async (
 export const getItems = async (
   ids: Array<Item["id"]>,
   options?: AxiosRequestConfig,
-): Promise<AxiosResponse<Item[]> | null> =>
+): Promise<Array<AxiosResponse<Item> | null>> =>
   await Promise.all(ids.map(async id => await getItem(id, options)));
+
+export const getSortedItemsByScore = async (
+  ids: Array<Item["id"]>,
+  options?: AxiosRequestConfig,
+): Promise<Array<Item | null>> => {
+  const response = await getItems(ids, options);
+
+  // Get items out of response
+  const items = response.map(item => item?.data ?? null);
+
+  // Sort items in ascending order and return
+  return items?.sort((a, b) => (a?.score ?? 0) - (b?.score ?? 0));
+};
