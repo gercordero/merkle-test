5280b40d6ca5b311ea7564c0da786e72cffa1613
German Cordero <gerrcordero@gmail.com>
2022-09-15 05:11:49 +0200

    Moving fetch logic to Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,16 @@
-// React
-import { useEffect } from "react";
-// Hooks
-import { useFetchReducer } from "@/hooks";
-// Services
-import { getRandomTopStories } from "@/services/topstoriesService/topstoriesService";
-// Types
-import type { TopStories } from "@/types/HackerAPITypes";
-import type { FetchReducerState } from "@/hooks/useFetchReducer/useFetchReducer";
+// Pages
+import Home from "@/pages/Home";
+// Components
+import Layout from "@/components/Layout/Layout";
 // Styles
 import "@/styles/main.scss";
 
-type RandomTopStories = TopStories["TopStories"] | null;
-
-const useFetchRandomTopStories = (): FetchReducerState<RandomTopStories> => {
-  const [state, dispatch] = useFetchReducer<RandomTopStories>();
-
-  useEffect(() => {
-    const fetchRandomTopStories = async (): Promise<void> => {
-      dispatch({ type: "loading" });
-      try {
-        const data = await getRandomTopStories(10);
-
-        dispatch({ type: "success", payload: data });
-      } catch (error) {
-        dispatch({ type: "error", payload: error as Error });
-      }
-    };
-
-    void fetchRandomTopStories();
-  }, [dispatch]);
-
-  return state;
-};
-
 const App = (): JSX.Element => {
-  const randomTopStories = useFetchRandomTopStories();
-
-  return <h1>APP</h1>;
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
 };
 
 export default App;
