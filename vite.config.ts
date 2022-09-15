8aca6199bfa5cec7f919acbf92ff4b22e4052eac
German Cordero <gerrcordero@gmail.com>
2022-09-15 05:18:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    logOverride: { "this-is-undefined-in-esm": "silent" },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
