cb97dcdc1f57467129434bd1780f02c3bf2c6316
German Cordero <gerrcordero@gmail.com>
2022-09-15 05:13:56 +0200

    Implementing Navbar

---
+++ src/components/Layout/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+// Styles
+import "./Navbar.scss";
+
+const Navbar = (): JSX.Element => (
+  <nav className="navbar">
+    <h3 className="navbar__logo">
+      <i>Hacker News</i>
+    </h3>
+  </nav>
+);
+
+export default Navbar;
